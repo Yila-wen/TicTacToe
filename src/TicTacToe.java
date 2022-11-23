7536460d650bc757b300df51c4ddec2bd7bdbf82
JiaXiangHuang <jhuang4836@bths.edu>
2022-11-22 19:55:23 -0500

    Finished 2d array, wincondition for 2d array,added toString, added to runner.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,13 +1,11 @@
-import java.util.Scanner;
 public class TicTacToe {
     public int tSpot;
     private int outcome = 0;
-    private int cpuCount = 0;
-    private int pCount = 1;
-    public int pInput = 0;
     public int pTotal = 0;
-    public int cpuTotal = 0;
-    private boolean play = false;
+    public int cpuTotal;
+    private int wins = 0;
+    private int loses = 0;
+    public boolean play = false;
     private boolean tLC = true;
     private boolean tC = true;
     private boolean tRC = true;
@@ -22,36 +20,37 @@ public class TicTacToe {
             {" "," "," ",""},
             {" "," "," ",""}
     };
-
-    Scanner s = new Scanner(System.in);
-
-
-    public TicTacToe(String choice){
-        cpuTotal = 0;
-        play = true;
-        if (choice.contains("c")){
-
-            }
+    public TicTacToe(){
         }
+    public void setPlay(){
+        tSpot = 9;
+        play = true;
+        }
+
     public String displayGrid(){
-        String s = "_____________\n";
+        String s = "_______\n";
         for (int r = 0;r<3;r++){
             for (int c = 0; c < 4;c++){
-                s+= "|" + grid[r][c];
                 if (c < 3) {
-                    s += "_" + grid[r][c];
+                    s += "|" + grid[r][c];
                 }
+                else s +=  grid[r][c]+"|";
             }
             s+="\n";
 
         }
-        s+= "-------------";
+        s+= "-------";
         return s;
     }
-    public void start(){
-
+    public void clownGrid(int row,int column,String input){
+        grid[row][column] = input;
     }
-    public int clown(){
+    public void playerGrid(int row,int column,String input){
+        grid[row][column] = input;
+    }
+    public void clown(String symbol){
+        int column = 0;
+        int row = 0;
         int check = 0;
         getOutcome();
         while (!spot(outcome) && check == 0){
@@ -59,15 +58,19 @@ public class TicTacToe {
             if (spot(outcome)) {
                 check++;
             }
-            if (winCondition(tSpot) == -1){
+            if (winCondition().equals(symbol)){
                 tSpot = 0;
+                loses ++;
                 play = false;
             }
     }
         cpuTotal += outcome;
-        cpuCount++;
         tSpot--;
-    return outcome;}
+        if (outcome%3==0)row = (outcome/3)-1;
+        else row = (outcome/3);
+        if (outcome%3==0) column = 2;
+        else column = (outcome%3)-1;
+    clownGrid(row,column,symbol);}
 
     private int mald(){
         outcome = 5;
@@ -82,88 +85,81 @@ public class TicTacToe {
 
     private boolean spot ( int spotnum ) {
         int check = 0;
-        if (spotnum == 1 && tLC == true){ tLC = false; check++;}
-        if (spotnum == 2 && tC == true){ tC = false; check++;}
-        if (spotnum == 3 && tRC == true){ tRC = false; check++;}
-        if (spotnum == 4 && mL == true){ mL = false; check++;}
-        if (spotnum == 5 && mC == true){ mC = false; check++;}
-        if (spotnum == 6 && mR == true){ mR = false; check++;}
-        if (spotnum == 7 && bLC == true){ bLC = false; check++;}
-        if (spotnum == 8 && bC == true){ bC = false; check++;}
-        if (spotnum == 9 && bRC == true){ bRC = false; check++;}
+        if (spotnum == 1 && tLC){ tLC = false; check++;}
+        if (spotnum == 2 && tC){ tC = false; check++;}
+        if (spotnum == 3 && tRC){ tRC = false; check++;}
+        if (spotnum == 4 && mL){ mL = false; check++;}
+        if (spotnum == 5 && mC){ mC = false; check++;}
+        if (spotnum == 6 && mR){ mR = false; check++;}
+        if (spotnum == 7 && bLC){ bLC = false; check++;}
+        if (spotnum == 8 && bC){ bC = false; check++;}
+        if (spotnum == 9 && bRC){ bRC = false; check++;}
         if (check == 1) {
-
+            tSpot--;
             return true;}
         else return false;
     }
-    public int winCondition(int number){
-        if (winCChecker()){
-        if (number < 5 && number != 0){
-        if (pTotal%6 == 0 || pTotal == 15){
-            System.out.println("Player wins");
-            return 1;
-        }
-        if (cpuTotal%6 == 0 || cpuTotal == 15){
-            System.out.println("Computer wins");
-            return -1;
-        }
-    }
-        if (number == 0){
-            return 0;
-        }}
-        return 5;
-    }
-    public boolean winCChecker(){
-        if (!(tLC || tC || tRC)){
-            return true;
-        }
-        else if (!(mL||mC||mR)){
-            return true;
-        }
-        else if (!(bLC||bC||bRC)){
-            return true;
-        }
-        else if (!(tLC||mL||bLC)){
-            return true;
-        }
-        else if (!(tC||mC||mL)){
-            return true;
-        }
-        else if (!(tRC||mR||bRC)){
-            return true;
-        }
-        else if(!(tRC||mC||bLC)){
-            return true;
-        }
-        else if (!(tLC||mC||bRC)){
-            return true;
-        }
-        else return false;
+    public String winCondition(){
+        if (tSpot<=4){
+                String c1 = grid[0][0]+grid[0][1]+grid[0][2];
+                String c2 = grid[1][0]+grid[1][1]+grid[1][2];
+                String c3 = grid[2][0]+grid[2][1]+grid[2][2];
+                String r1 = grid[0][0]+grid[1][0]+grid[2][0];
+                String r2 = grid[0][1]+grid[1][1]+grid[2][1];
+                String r3 = grid[0][2]+grid[1][2]+grid[2][2];
+                String ld = grid[0][0]+grid[1][1]+grid[2][2];
+                String rd = grid[0][2]+grid[1][1]+grid[2][0];
+
+                if(c1.equals("XXX")) return "X";
+                if(c2.equals("XXX")) return "X";
+                if(c3.equals("XXX")) return "X";
+                if(r1.equals("XXX")) return "X";
+                if(r2.equals("XXX")) return "X";
+                if(r3.equals("XXX")) return "X";
+                if(ld.equals("XXX")) return "X";
+                if(rd.equals("XXX")) return "X";
+
+                if(c1.equals("OOO")) return "O";
+                if(c2.equals("OOO")) return "O";
+                if(c3.equals("OOO")) return "O";
+                if(r1.equals("OOO")) return "O";
+                if(r2.equals("OOO")) return "O";
+                if(r3.equals("OOO")) return "O";
+                if(ld.equals("OOO")) return "O";
+                if(rd.equals("OOO")) return "O";
+            }
+        return "";
     }
 
-    public void pInputChecker(int pInput){
+    public void pInputChecker(int pInput,String preference){
         int pcheck =0;
+        int column = 0;
+        int row = 0;
         if (play){
-        while (!spot(outcome) && pcheck == 0){
-            if (spot(pInput)){
-                pcheck++;
-                pCount++;
-                pTotal += pInput;
-                if (winCondition(tSpot) == 1){
+            while (!spot(pInput) && pcheck == 0){
+                if (spot(pInput)){
+                    pcheck++;
+                    pTotal += pInput;
+                    if (pInput%3==0)row = (pInput/3)-1;
+                    else row = (pInput/3);
+                    if (pInput%3==0) column = 2;
+                    else column = (pInput%3)-1;
+                    playerGrid(row,column,preference);
+                    if (winCondition().equals(preference)){
                     tSpot = 0;
-                    winCondition(pTotal);
+                    wins ++;
                     play = false;
                 }
             }
         }
-    }}
+        }
+    }
 
     private void getOutcome(){
         outcome = (int)((Math.random()*9)+1);
     }
-    public String pInput(){
-        return "Please input your choice.";
+    public String toString(){return "Wins: "+wins +"\n"+"Loses: " +loses;}
 
-    }
+
 
 }
--- src/TicTacToeRunner.java
+++ src/TicTacToeRunner.java
@@ -2,35 +2,61 @@ import java.util.Scanner;
 
 public class TicTacToeRunner {
     public static void main(String[] args) {
+        TicTacToe game1 = new TicTacToe();
         Scanner s = new Scanner(System.in);
-        System.out.println("Welcome to TicTacToe, if you don't know the rules search it up."+ "\n"
-                + "Please type in either \"clown\" or \"mald\" to choose the difficulty you want.");
+        String clownPref = "X";
+        String answer = "y";
+
+
+        System.out.println("Welcome to TicTacToe, A Grid will be displayed with each box representing a different " +
+                "number(1-9), from left to right."+ "\n" + "Would you like to be \"X\" or \"O\"?");
+        String preference = s.nextLine();
+
+        while(!preference.equals("X")&&!preference.equals("x")&&!preference.equals("O")&&!preference.equals("o"))
+        {
+            System.out.println("Let's try again, Would you like to be \"X\" or \"O\"?");
+            preference = s.nextLine();
+        }
+        if (preference.equals("X")||preference.equals("x")) clownPref = "O";
+        if (preference.equals("x")) preference = "X";
+        else if (preference.equals("o")) preference = "O";
+
+        System.out.println("Please type in either \"clown\" or \"mald\" to choose the difficulty you want.");
         String choice = s.nextLine();
-        if (choice.contains("c")) {
 
 
-            TicTacToe game1 = new TicTacToe(choice);
+        if (choice.contains("c")||choice.contains("C")) {
+
+            game1.setPlay();
+            while(game1.play){
             System.out.println(game1.displayGrid());
-            System.out.println("Please input your choice");
+                if (game1.winCondition().equals(preference)) System.out.println("You Won!");
+                else if (game1.winCondition().equals(clownPref)) System.out.println("You Lost!");
+                else if (game1.tSpot==0) System.out.println("It's a Draw!");
+
+            System.out.println("Please choose a box");
             int pInput = s.nextInt();
-            game1.pInputChecker(pInput);
-            System.out.println(game1.clown());
 
+                game1.pInputChecker(pInput,preference);
+            System.out.println(game1.displayGrid());
+                if (game1.winCondition().equals(preference)){
+                    System.out.println("You Won!");
+                }
+                else if (game1.winCondition().equals(clownPref)){
+                    System.out.println("You Lost!");
+                }
+                else if (game1.tSpot==0){
+                    System.out.println("It's a Draw!");
+                }
 
-
-
-
-        if (game1.winCondition(game1.cpuTotal)==-1){
-            System.out.println("You Lost!");
-        }
-        else if (game1.winCondition(game1.pTotal)==1){
-            System.out.println("You Won!");
-        }
-        else if (game1.winCondition(game1.cpuTotal)!=1 && game1.winCondition(game1.pTotal)!=1){
-        if (game1.winCondition(game1.tSpot)==0){
-            System.out.println("It's a Draw!");
-        }
-        }
+            game1.clown(clownPref);
 
     }
+            System.out.println("Would you like to play again?");
+            answer = s.nextLine();
+
+        }
+
+
+        System.out.println("Here are your stats. \n" + game1.toString());
 }}
